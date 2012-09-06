ceecaba8302a84287e2d184bde1cd822abd02d27
Niels Poulsen <nielssonnich@gmail.com>
2012-09-06 16:41:30 +0200

    Made username and authtoken into constants.

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
+
+define('USERNAME', '');
+define('AUTHTOKEN', '');
+
 include 'SG-iCalendar/SG_iCal.php';
 
-$ical = new SG_iCalReader("http://sict.moodle.aau.dk/calendar/export_execute.php?preset_what=all&preset_time=recentupcoming&username=nspo11%40student.aau.dk&authtoken=37c6068f91a65bb349ac4fe65933bd1dcc3a8928");
+$ical = new SG_iCalReader(
+  'http://sict.moodle.aau.dk/calendar/export_execute.php'
+  . '?preset_what=all&preset_time=recentupcoming&username='
+  . USERNAME . '&authtoken='
+  . AUTHTOKEN
+);
 
 include 'IcalGenerator.php';
 
