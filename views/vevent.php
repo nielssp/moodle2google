bc37c891dad7ab9d6468c5f9a1510878c40cbed3
Niels Poulsen <nielssonnich@gmail.com>
2012-09-06 15:43:29 +0200

    License...

--- views/vevent.php
+++ views/vevent.php
@@ -7,6 +7,9 @@ CLASS:PUBLIC
 LAST-MODIFIED:<?php echo $event->getProperty('last-modified'); ?> 
 DTSTAMP:<?php echo $event->getProperty('dtstamp'); ?> 
 DTSTART:<?php echo $event->getProperty('dtstart'); ?> 
+<?php if ($event->getProperty('dtend') != null): ?>
 DTEND:<?php echo $event->getProperty('dtend'); ?> 
+<?php endif; ?>
 CATEGORIES:<?php echo $event->getProperty('categories'); ?> 
 END:VEVENT
+
