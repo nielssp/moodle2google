4c5226a9f12d6840ce8ed4e295b0c39b5d5fe849
Niels Poulsen <nielssonnich@gmail.com>
2012-09-06 15:54:05 +0200

    Fixed problems.. Now definitely works.

--- views/vcalendar.php
+++ views/vcalendar.php
@@ -1,7 +1,7 @@
-BEGIN:VCALENDAR
-METHOD:PUBLISH
-PRODID:-//moodle2google//NONSGML 0.1//EN
-VERSION:2.0
+BEGIN:VCALENDAR<?php echo "\r\n"; ?>
+METHOD:PUBLISH<?php echo "\r\n"; ?>
+PRODID:-//moodle2google//NONSGML 0.1//EN<?php echo "\r\n"; ?>
+VERSION:2.0<?php echo "\r\n"; ?>
 <?php
 foreach ($events as $event) {
   $this->viewEvent($event);
--- views/vevent.php
+++ views/vevent.php
@@ -1,15 +1,14 @@
-BEGIN:VEVENT
-UID:<?php echo $event->getUid(); ?> 
-SUMMARY:<?php echo $course; ?> 
-DESCRIPTION:<?php echo $note . '\n' . $thing . '\n' . $event->getDescription(); ?> 
-LOCATION:<?php echo $place; ?> 
-CLASS:PUBLIC
-LAST-MODIFIED:<?php echo $event->getProperty('last-modified'); ?> 
-DTSTAMP:<?php echo $event->getProperty('dtstamp'); ?> 
-DTSTART:<?php echo $event->getProperty('dtstart'); ?> 
+BEGIN:VEVENT<?php echo "\r\n"; ?>
+UID:<?php echo $event->getUid(); ?> <?php echo "\r\n"; ?>
+SUMMARY:<?php echo $course; ?> <?php echo "\r\n"; ?>
+DESCRIPTION:<?php echo $note . '\n' . $thing . '\n' . $event->getDescription(); ?> <?php echo "\r\n"; ?>
+LOCATION:<?php echo $place; ?> <?php echo "\r\n"; ?>
+CLASS:PUBLIC<?php echo "\r\n"; ?>
+LAST-MODIFIED:<?php echo $event->getProperty('last-modified'); ?><?php echo "\r\n"; ?>
+DTSTAMP:<?php echo $event->getProperty('dtstamp'); ?><?php echo "\r\n"; ?>
+DTSTART:<?php echo $event->getProperty('dtstart'); ?><?php echo "\r\n"; ?>
 <?php if ($event->getProperty('dtend') != null): ?>
-DTEND:<?php echo $event->getProperty('dtend'); ?> 
+DTEND:<?php echo $event->getProperty('dtend'); ?><?php echo "\r\n"; ?>
 <?php endif; ?>
-CATEGORIES:<?php echo $event->getProperty('categories'); ?> 
-END:VEVENT
-
+CATEGORIES:<?php echo $event->getProperty('categories'); ?><?php echo "\r\n"; ?>
+END:VEVENT<?php echo "\r\n"; ?>
