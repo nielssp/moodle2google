07712100f27abf13472fd3c429b932b4d89027d1
Niels Poulsen <nielssonnich@gmail.com>
2012-09-12 21:03:44 +0300

    Added posibility to remove selected courses

--- IcalGenerator.php
+++ IcalGenerator.php
@@ -3,11 +3,24 @@
 class IcalGenerator {
 
   private $text = false;
+  
+  private $remove = array();
 
   public function __construct() {
     if (isset($_GET['debug'])) {
       $this->text = true;
     }
+    if (isset($_GET['remove'])) {
+      $this->remove = $this->csvToArray($_GET['remove']);
+    }
+    else {
+      $this->remove = array('AD2', 'DEB2');
+    }
+  }
+  
+  private function csvToArray($input) {
+    // TODO: Improve? (like str_getcsv()? (PHP5.3+))
+    return explode(',', $input);
   }
   
   public function generate(SG_iCalReader $ical) {
@@ -27,21 +40,18 @@ class IcalGenerator {
   }
 
   public function viewEvent(SG_iCal_VEvent $event) {
+    $summary = $event->getSummary();
+    foreach ($this->remove as $needle) {
+      if (strpos($summary, $needle) !== false) {
+        return;
+      }
+    }
     $summaryArray = explode(' - ', $event->getSummary());
     list( , $course) = explode(' ', $summaryArray[0], 2);
     preg_match('/^(.+?) (\(.+?\))/', $course, $matches);
     $course = $matches[1];
     $thing = $matches[2];
-    $note = $summaryArray[1];
-    if (strpos($note, 'AD2') !== false) {
-      return;
-    }
-    if (strpos($note, 'DEB2') !== false) {
-      return;
-    }
-    if (strpos($note, 'SU(ENG)') !== false) {
-      return;
-    }
+    $note = $summaryArray[1]
     list( , $place) = explode(' ', $summaryArray[4], 2);
     include 'views/vevent.php';
   }
